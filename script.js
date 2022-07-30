24fdc27fd33856de248a67ddb05aa4bab655e3eb
Adam-Smith-Coder <adcollects@gmail.com>
2022-07-30 10:20:57 +0100

    Create radio buttons for HTML doc for colour choice
    Update script.js with multicoloured choice option for background
    Tweak this to include an if statement based off whether black of multicoloured
    is selected

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ function makeRows(rows, cols) {
     sketchbox.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
+        cell.addEventListener('mouseover', colourChange)
         sketchbox.appendChild(cell).className = "grid-item";
   };
 };
+
+const radioButtons = document.querySelectorAll('input[name="colours"]');
+
+function colourChange (el) {
+    if (radioButtons.value = 'black') {
+        Object.assign(el.target.style, {
+            backgroundColor: black
+        })
+            console.log(backgroundColor)
+    } else if (radioButtons.value = 'multi') {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+        Object.assign(el.target.style, {
+            backgroundColor: color
+        })
+        console.log(backgroundColor)
+    }
+}
